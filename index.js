12f14f34eade80233fe09685c0def8943129c4e0
Jordanks93 <69221166+jordanks93@users.noreply.github.com>
2020-12-15 18:08:56 -0500

    added questions and updated gitignore with DS_Store

--- index.js
+++ index.js
@@ -5,7 +5,41 @@ var fs = require("fs");
 // array of questions for user
 const questions = [
 
+    {
+        type: "input",
+        message: "What is the title of your project?",
+        name: "title",
+        default: "ReadME Generator"
+    },
+
+    {
+        type: "input",
+        message: "Give a description of your project:",
+        name: "description",
+        default: "This project will generate a readME markdown file based on the users input."
+    },
+
+    {
+        type: "input",
+        message: "Provide installion instructions for your project:",
+        name: "installations",
+    },
+
+    {
+        type: "input",
+        message: "Is there any inportant usage information for your project?",
+        name: "usage",
+        default: "This project is used to automatically generate a readME file. In the terminal, make sure you are in the projects file directory. Then type node + 'filename'. You will then be prompted with questions to generate the readME.md for your project."
+    },
+
+    {
+        type: "input",
+        message: "List the contributors for this project:",
+        name: "contributions",
+    }
+
 ];
+
 
 // function to write README file
 function writeToFile(fileName, data) {
